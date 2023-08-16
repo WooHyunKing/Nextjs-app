809729aa3e30a5a4e9d40a815939f8a741711c74
신 우현 <woo_hyun_1@naver.com>
2023-08-16 14:14:04 +0900

    [Chap-10] Implement post function with client component

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,7 +1,58 @@
-import React from "react";
+"use client";
+import { ITopic } from "@/interfaces/Topic";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
 
 const CreatePage = () => {
-  return <div>This is Create Page !</div>;
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const router = useRouter();
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const response = await axios.post(
+      "http://localhost:9999/topics/",
+      { title: title, body: body },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    console.log(response);
+    const newData = response.data.id;
+    router.push(`read/${newData}`);
+    router.refresh();
+  };
+  return (
+    <form onSubmit={onSubmit}>
+      <p>
+        <input
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setTitle(e.target.value)
+          }
+          type="text"
+          name="title"
+          placeholder="title"
+          value={title}
+        ></input>
+      </p>
+      <p>
+        <textarea
+          onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) =>
+            setBody(e.target.value)
+          }
+          name="body"
+          placeholder="body"
+          value={body}
+        ></textarea>
+      </p>
+      <p>
+        <input type="submit" value="create"></input>
+      </p>
+    </form>
+  );
 };
 
 export default CreatePage;
